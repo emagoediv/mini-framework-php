c6ef70d2f27a9ca964fef7bb9dc0b96a5ff7e4d3
emagoediv <emagoedivplay@gmail.com>
2022-02-16 08:11:31 -0300

    Sistema de rotas simples

---
+++ app/config/Router.php
@@ -0,0 +1,43 @@
+<?php
+
+$this->get('/home', function(){
+    echo "
+    <div>
+        <h1>Home</h1>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+    </div>";
+});
+
+
+$this->get('/about/test', function(){
+    echo "
+    <div>
+        <h1>about/test</h1>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+    </div>";
+});
+
+$this->get('/categorias', function(){
+    echo "
+    <div>
+        <h1>categorias</h1>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+        <button>aopa</button>
+    </div>
+    ";
+
+});
